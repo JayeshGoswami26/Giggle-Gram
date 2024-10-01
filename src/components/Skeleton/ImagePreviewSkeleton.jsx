a8f70438a1dc2fdc9df76183c88b8b521f5832d6
Jayesh Goswami <goswamijayesh54@gmail.com>
2024-10-01 12:06:27 +0530

    Readme and skeleton added

---
+++ src/components/Skeleton/ImagePreviewSkeleton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ImagePreviewSkeleton() {
+  return (
+    <div className='w-full h-[18rem] skeleton-gradient bg-gray-200/5 rounded-[1rem] '></div>
+  )
+}
+
+export default ImagePreviewSkeleton
